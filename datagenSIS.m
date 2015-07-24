f8c82059dc4bc69872c7d352181871ab4b8df3c8
Andrewpensoneault <ap21@geneseo.edu>
2015-07-24 19:16:08 -0400

    Generates noisy data for SIS ODE model

--- datagenSIS.m
+++ datagenSIS.m
@@ -2,7 +2,7 @@ function data = datagenSIS(params,pop,IC,tstop,r)
 % Numerical Solutions
 parameters=[params pop];
 options = [];
-[~,y]=ode45(@sis_ode_6_19,0:1:tstop,IC,options,parameters);
+[~,y]=ode45(@sis_ode_6_19,1:1:tstop,IC,options,parameters);
 % Generate noisy data in I
 data = y(:,1) + r.*randn(size(y(:,1),1),1);
 end
