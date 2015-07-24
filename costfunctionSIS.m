6d414f250b53cad5250751de5bdde71d49847a0d
Andrewpensoneault <ap21@geneseo.edu>
2015-07-24 19:15:42 -0400

    Cost function based off GLS

--- costfunctionSIS.m
+++ costfunctionSIS.m
@@ -2,16 +2,10 @@ function val = costfunctionSIS(data,timespan,init_vec,theta,N)
 % Theta = parameters
 
 options = [];
-[~,y]=ode45(@SISModel,0:1:timespan,init_vec,[],theta,N); 
-
-% Cost Function model form
+[~,y]=ode45(@SISModel,1:1:length(data),init_vec,[],theta,N); 
+% Cost Function
 if length(y)<length(data)
     y(length(y):length(data))=0;
 end
     val = sum((data-y).^2./y.^2);
-
-
-function ydot=SISModel(t,y,a,N)
-ydot=[a(1)*y*(N-y)/N-a(2)*y];
-end
 end
