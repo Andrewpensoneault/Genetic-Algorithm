b799b7b1415c6df39b52b2d5f1cb1c5d6c82c779
Andrewpensoneault <ap21@geneseo.edu>
2015-07-24 19:17:35 -0400

    Initialized SIS data

--- Initialize.m
+++ Initialize.m
@@ -1,9 +1,16 @@
 %Data generation
 clc
 clear all
+ranseed=2;
+rand('seed',ranseed)
 params=[.125,.1];
 timespan=365;
 pop=1000;
 IC=5;
-r=sqrt(1);
-data=datagenSIS(params,pop,IC,timespan,r);
+%% Deterministic
+%r=sqrt(1);
+%data=datagenSIS(params,pop,IC,timespan,r);
+%% Stochastic
+nsamp=1;
+data=stochasticSIS_gen_data(nsamp,timespan,[pop-IC,IC],params);
+data=data(find(data(:,2)~=0),2);
